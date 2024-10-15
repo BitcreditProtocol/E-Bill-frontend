4081f5635125f63ddfdbfc728eab2ae407d3202e
Guilherme Salomão <cryptosalomao@gmail.com>
2024-10-15 01:43:17 -0300

    initial version for onboard flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
-import { Button } from "./components/ui/button";
 import DefaultLayout from "./layouts/Default";
-import HomeLogo from "./assets/home-logo.svg";
+import CreateIdentity from "./pages/onboarding/CreateIdentity";
+import ConnectCompany from "./pages/onboarding/ConnectCompany";
+import ProfileInfo from "./pages/onboarding/ProfileInfo";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <DefaultLayout>
-      <div className="flex flex-col gap-[8px]">
-        <div className="flex flex-col">
-          <img src={HomeLogo} alt="home-logo" />
-        </div>
-        <div className="flex flex-col items-center gap-[24px]">
-          <span className="text-sm text-[#71717A] font-normal">
-            Bitcoin for the Real Economy
-          </span>
-          <Button className="bg-[#F7931A] hover:bg-[#F2930a] w-full hover:border-transparent rounded-none">
-            Get started
-          </Button>
-          <div className="flex flex-col items-center gap-[24px]">
-            <p className="text-sm font-normal">
-              Don't have an account?{" "}
-              <span className="font-semibold">Sign up</span>
-            </p>
-            <span className="text-sm font-semibold">Recover account</span>
-          </div>
-        </div>
-      </div>
+      <Home />
+      {/* <CreateIdentity /> */}
+      {/* <ConnectCompany /> */}
+      {/* <ProfileInfo /> */}
     </DefaultLayout>
   );
 }
