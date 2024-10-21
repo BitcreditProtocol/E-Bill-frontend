b51e430338c3b7835a9cbdf22af87648242bb3f2
Guilherme Salomão <cryptosalomao@gmail.com>
2024-10-21 11:35:11 -0300

    chore: initial service integration

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,7 @@
 export const copyToClipboard = (value: string) =>
   navigator.clipboard.writeText(value);
+
+export const isPWA = () =>
+  window.matchMedia("(display-mode: standalone)").matches ||
+  "standalone" in window.navigator ||
+  document.referrer.includes("android-app://");
