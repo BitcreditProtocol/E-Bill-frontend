b46e984e8ae87a1feb73f72f58ad222568ce3020
Guilherme Salomão <cryptosalomao@gmail.com>
2024-10-21 10:26:20 -0300

    chore: setup react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+import DefaultLayout from "./layouts/Default.tsx";
+import CreateIdentity from "./pages/onboarding/CreateIdentity";
+import ConnectCompany from "./pages/onboarding/ConnectCompany";
+import ProfileInfo from "./pages/onboarding/ProfileInfo";
+import Home from "./pages/Home";
+
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/create-identity",
+    element: <CreateIdentity />,
+  },
+  {
+    path: "/connect-company",
+    element: <ConnectCompany />,
+  },
+  {
+    path: "/profile-info",
+    element: <ProfileInfo />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <DefaultLayout>
+      <RouterProvider router={router} />
+    </DefaultLayout>
+  </StrictMode>
+);
