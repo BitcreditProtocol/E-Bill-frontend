1137fc43c798706d3a3c0c4bce1f291de13b9488
MT <tymkomykyta2108@gmail.com>
2024-10-29 10:40:25 +0100

    Try to add codecov.

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
     test: {
+        include: ['tests/**/*.test.ts'], // Adjust this to your test file location
         coverage: {
             provider: 'v8',
             reporter: ['text', 'json', 'lcov'],
-            reportsDirectory: './coverage', // default path for coverage output
+            reportsDirectory: './coverage',
         },
     },
 });
