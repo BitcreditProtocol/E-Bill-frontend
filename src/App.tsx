7777170dc20aefdaf89812b998e5a76ac1b921dc
Guilherme Salomão <cryptosalomao@gmail.com>
2024-10-29 12:50:23 -0300

    chore: fix deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import DefaultLayout from "./layouts/Default";
-import CreateIdentity from "./pages/onboarding/CreateIdentity";
 import ConnectCompany from "./pages/onboarding/ConnectCompany";
-import ProfileInfo from "./pages/onboarding/ProfileInfo";
-import Home from "./pages/Home";
 
 function App() {
   return (
     <DefaultLayout>
-      {/* <Home /> */}
-      {/* <CreateIdentity /> */}
       <ConnectCompany />
-      {/* <ProfileInfo /> */}
     </DefaultLayout>
   );
 }
