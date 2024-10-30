774238380da801c4170a623877638766f44c8761
Guilherme Salomão <cryptosalomao@gmail.com>
2024-10-30 15:55:39 -0300

    chore: add paste actions

--- src/pages/RecoverWithSeedPhrase.tsx
+++ src/pages/RecoverWithSeedPhrase.tsx
@@ -1,22 +1,66 @@
+import { useState } from "react";
 import { FormattedMessage } from "react-intl";
-import { ChevronLeftIcon, CopyIcon } from "lucide-react";
+import { ChevronLeftIcon, CircleXIcon, CopyIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-function WordField({ id }: { id: number }) {
+type WordFieldProps = {
+  id: number;
+  value: string;
+  onChange: (value: string) => void;
+  onPaste: (e: React.ClipboardEvent<HTMLInputElement>) => void;
+};
+
+function WordField({ id, value, onChange, onPaste }: WordFieldProps) {
   return (
-    <div className="flex justify-center items-center bg-[#EEEBE3] p-2 rounded-[8px]">
-      <div className="flex justify-center items-center w-[50px] h-[30px] bg-[#D1CCC1] rounded-[4px] font-medium text-[14px]">
+    <div className="flex justify-center items-center gap-2 h-[46px] bg-elevation-200 py-1 pl-2 pr-4 rounded-[8px] border-[1px] border-[#1B0F0014]">
+      <div className="flex justify-center items-center p-3 w-[30px] h-[30px] bg-divider-100 rounded-[4px] font-medium text-[14px]">
         {id}
       </div>
-      <input className="w-full py-2 px-4 bg-transparent outline-none" />
+      <input
+        className="w-full bg-transparent outline-none text-text-300 text-[14px] font-medium leading-[14px]"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        onPaste={onPaste}
+      />
     </div>
   );
 }
 
 export default function RecoverWithSeedPhrase() {
+  const [seedWords, setSeedWords] = useState(Array(12).fill(""));
+
+  const handleInputChange = (index: number, newValue: string) => {
+    const updatedWords = [...seedWords];
+    updatedWords[index] = newValue;
+    setSeedWords(updatedWords);
+  };
+
+  const handlePaste = async (e: React.ClipboardEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const text = e.clipboardData.getData("Text");
+    const words = text.split(" ").slice(0, 12);
+
+    const paddedWords = words.concat(Array(12 - words.length).fill(""));
+    setSeedWords(paddedWords);
+  };
+
+  const pasteFromClipboard = async () => {
+    const text = await navigator.clipboard.readText();
+    const words = text.split(" ").slice(0, 12);
+
+    const paddedWords = words.concat(Array(12 - words.length).fill(""));
+    setSeedWords(paddedWords);
+  };
+
+  const clearAllFields = () => {
+    setSeedWords(Array(12).fill(""));
+  };
+
+  const hasAnyFieldFilled = seedWords.some((word) => word !== "");
+
   return (
-    <div className="flex flex-col min-h-fit h-screen gap-20 py-12 px-6">
-      <div className="flex flex-col items-center gap-20 text-center">
+    <div className="flex flex-col min-h-fit h-screen gap-10 py-12 px-6">
+      <div className="flex flex-col items-center gap-10 text-center">
         <div className="flex w-full">
           <button className="flex items-center justify-center w-8 h-8 bg-[#1B0F00]/20 rounded-full border-[1px] border-[#1B0F00]/6">
             <ChevronLeftIcon width={16} strokeWidth={1} color="#1B0F00" />
@@ -25,27 +69,71 @@ export default function RecoverWithSeedPhrase() {
 
         <div className="flex flex-col gap-2">
           <h1 className="font-sans font-medium text-[24px] mx-3">
-            Recover with seed phrase
+            <FormattedMessage
+              id="pages.recoverWithSeedPhrase.title"
+              defaultMessage="Recover with seed phrase"
+              description="Heading copy for the recover with seed phrase page"
+            />
           </h1>
           <span className="font-normal text-text-200 text-[16px]">
-            Please paste in your seed phrase to recover your identity
+            <FormattedMessage
+              id="pages.recoverWithSeedPhrase.description"
+              defaultMessage="Please paste in your seed phrase to recover your identity"
+              description="Description copy for the recover with seed phrase page"
+            />
           </span>
         </div>
       </div>
 
-      <div className="grid grid-cols-2 gap-4">
-        {Array.from({ length: 12 }, (_, i) => (
-          <WordField key={i + 1} id={i + 1} />
-        ))}
+      <div className="flex flex-col gap-11">
+        <div className="grid grid-cols-2 gap-4">
+          {seedWords.map((word, i) => (
+            <WordField
+              key={i}
+              id={i + 1}
+              value={word}
+              onChange={(newValue) => handleInputChange(i, newValue)}
+              onPaste={handlePaste}
+            />
+          ))}
+        </div>
+
+        {hasAnyFieldFilled ? (
+          <Button
+            variant="link"
+            className="gap-2 text-[#1B0F0080] text-[14px] font-medium"
+            onClick={clearAllFields}
+          >
+            <CircleXIcon width={20} color="#1B0F0080" strokeWidth={1} />
+            <FormattedMessage
+              id="pages.recoverWithSeedPhrase.clear"
+              defaultMessage="Clear all"
+              description="Button to clear all fields in the recover with seed phrase page"
+            />
+          </Button>
+        ) : (
+          <Button
+            variant="link"
+            className="gap-2 text-[#1B0F0080] text-[14px] font-medium"
+            onClick={pasteFromClipboard}
+          >
+            <CopyIcon width={20} color="#1B0F0080" strokeWidth={1} />
+            <FormattedMessage
+              id="pages.recoverWithSeedPhrase.paste"
+              defaultMessage="Paste from clipboard"
+              description="Button to paste seed phrase from clipboard in the recover with seed phrase page"
+            />
+          </Button>
+        )}
       </div>
 
       <div className="flex flex-col gap-3">
-        <div className="flex gap-1 items-center justify-center">
-          <CopyIcon width={20} color="#1B0F00" strokeWidth={1} />
-          Paste from clipboard
-        </div>
         <Button className="w-full bg-text-300 text-white font-medium rounded-[8px] py-[24px] px-[32px]">
-          Restore identity
+          <FormattedMessage
+            id="pages.recoverWithSeedPhrase.restoreIdentity"
+            defaultMessage="Restore identity"
+            description="Button to restore identity in the recover with seed phrase page"
+          />
         </Button>
       </div>
     </div>
