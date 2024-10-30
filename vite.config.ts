f741e4a00c8f9ac93652cc370123148597ae5b01
Guilherme Salomão <cryptosalomao@gmail.com>
2024-10-30 00:38:53 -0300

    chore: add test setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-
 import { configDefaults } from "vitest/config";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
