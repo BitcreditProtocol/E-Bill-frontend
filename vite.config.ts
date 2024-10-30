c2f116a6e66c98989689ea1bf0be5dc2222d797e
Guilherme Salomão <cryptosalomao@gmail.com>
2024-10-30 00:37:09 -0300

    chore: add test setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
 import { defineConfig } from "vite";
 import { configDefaults } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  assetsInclude: ["**/*.woff2"],
   test: {
-    include: ["tests/**/*.test.ts"], // Adjust this to your test file location
+    include: ["tests/**/*.test.ts"],
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "lcov"],
       reportsDirectory: "./coverage",
     },
+    exclude: [...configDefaults.exclude],
   },
-  assetsInclude: ["**/*.woff2"],
 });
