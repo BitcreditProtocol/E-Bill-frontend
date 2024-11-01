73cdd0b1ac54444c58a37551a61b30a4c8f69438
Guilherme Salomão <cryptosalomao@gmail.com>
2024-11-01 18:29:55 -0300

    chore: create identity start page

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,7 +2,7 @@ const LOGIN = "/login";
 const UNLOCK = "/unlock";
 const RESTORE_WITH_PRIVATE_KEY = "/private-key";
 const RESTORE_WITH_SEED_PHRASE = "/seed-phrase";
-
+const CREATE_IDENTITY = "/create-identity";
 const HOME = "/home";
 
 export default {
@@ -11,4 +11,5 @@ export default {
   HOME,
   RESTORE_WITH_PRIVATE_KEY,
   RESTORE_WITH_SEED_PHRASE,
+  CREATE_IDENTITY,
 };
