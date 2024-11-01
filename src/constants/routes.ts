4dbf6b454d6e6dc927e6296c620a083ddcdbf8ab
Guilherme Salomão <cryptosalomao@gmail.com>
2024-11-01 14:12:41 -0300

    chore: create home page component (#21)

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -3,9 +3,12 @@ const UNLOCK = "/unlock";
 const RESTORE_WITH_PRIVATE_KEY = "/private-key";
 const RESTORE_WITH_SEED_PHRASE = "/seed-phrase";
 
+const HOME = "/home";
+
 export default {
   LOGIN,
   UNLOCK,
+  HOME,
   RESTORE_WITH_PRIVATE_KEY,
   RESTORE_WITH_SEED_PHRASE,
 };
