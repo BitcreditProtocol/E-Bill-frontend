8b4e5b73b5029aa403d59fcd37f9c0bb3908ad1d
nafisreza <bigganinafis@gmail.com>
2024-11-17 19:40:45 +0600

    fix: content-box for button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap font-medium transition-all ease-in-out focus-visible:outline-none disabled:cursor-not-allowed",
+  "inline-flex items-center justify-center whitespace-nowrap font-medium transition-all ease-in-out focus-visible:outline-none focus-visible:box-content disabled:cursor-not-allowed",
   {
     variants: {
       variant: {
