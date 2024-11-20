b3148b741d3f0194b580a7787e54fe727e9ef91a
matosGu1lherme <guimatos.com@gmail.com>
2024-11-20 16:26:28 -0300

    add datePicker componente

---
+++ src/pages/TestePage.tsx
@@ -0,0 +1,14 @@
+import { DatePickerDemo } from "@/components/ui/DatePickerDemo";
+import React from "react";
+
+const TestPage = () => {
+  return (
+    <div className="h-full">
+      <h1>Página de Teste</h1>
+      <p>Esta é uma página de teste para o novo componente.</p>
+      <DatePickerDemo></DatePickerDemo>
+    </div>
+  );
+};
+
+export default TestPage;
