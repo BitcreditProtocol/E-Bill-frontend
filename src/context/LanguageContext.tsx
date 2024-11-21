e62da38478e01d322339e39a4e0496013c887f40
MT <tymkomykyta2108@gmail.com>
2024-11-21 14:44:34 +0100

    Add en folder.

--- src/context/LanguageContext.tsx
+++ src/context/LanguageContext.tsx
@@ -2,7 +2,7 @@ import { IntlProvider } from "react-intl";
 import { createContext, useContext, useState } from "react";
 import { detectBrowserLanguage } from "@/utils";
 
-import en from "@/i18n/en/en.json";
+import en from "@/i18n/en/test.json";
 
 const DEFAULT_LOCALE = "en";
 
