bbb26f4b8fcdd1f9491f75a97f4701222fd79207
nafisreza <bigganinafis@gmail.com>
2024-11-26 10:01:23 +0600

    fix: use cn/cva for variants

--- src/components/ui/search.tsx
+++ src/components/ui/search.tsx
@@ -1,6 +1,8 @@
 import { SearchIcon } from "lucide-react";
 import { useRef } from "react";
 import { useIntl } from "react-intl";
+import { cva } from "class-variance-authority";
+import { cn } from "@/lib/utils";
 
 interface SearchProps {
   onSearch: (query: string) => void;
@@ -8,14 +10,24 @@ interface SearchProps {
   size?: "xs" | "sm" | "md" | "lg";
 }
 
-const sizeClasses = {
-  xs: "p-2 text-xs",
-  sm: "px-4 py-3 text-sm",
-  md: "p-4 text-sm",
-  lg: "px-4 py-5 text-sm",
-};
+const searchVariants = cva(
+  "flex items-center gap-2 bg-elevation-50 border-[1px] border-divider-75 rounded-[8px] transition-all ease-in-out duration-200    hover:bg-elevation-250 hover:border-divider-50 focus-within:bg-elevation-250 focus-within:border-divider-300",
+  {
+    variants: {
+      size: {
+        xs: "p-2 text-xs",
+        sm: "px-4 py-3 text-sm",
+        md: "p-4 text-sm",
+        lg: "px-4 py-5 text-sm",
+      },
+    },
+    defaultVariants: {
+      size: "md",
+    },
+  }
+);
 
-function Search({ onSearch, placeholder, size = "md" }: SearchProps) {
+function Search({ onSearch, placeholder, size }: SearchProps) {
   const intl = useIntl();
   const searchFieldRef = useRef<HTMLInputElement>(null);
 
@@ -30,10 +42,7 @@ function Search({ onSearch, placeholder, size = "md" }: SearchProps) {
   };
 
   return (
-    <div
-      onClick={focusSearchField}
-      className={`flex items-center gap-2 bg-elevation-50 border-[1px] border-divider-75 rounded-[8px] ${sizeClasses[size]} hover:bg-elevation-250 hover:border-divider-50 focus-within:bg-elevation-250 focus-within:border-divider-300 transition-all ease-in-out duration-200`}
-    >
+    <div onClick={focusSearchField} className={cn(searchVariants({ size }))}>
       <SearchIcon
         className={`${size === "xs" ? "h-4 w-4" : "h-5 w-5"} text-text-300`}
       />
