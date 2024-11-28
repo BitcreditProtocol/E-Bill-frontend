ca8dbe6c11e84e66d8fab32f641164c74773b8d4
mjkeaton <michaeljkeaton@protonmail.com>
2024-11-28 11:31:48 +0100

    chore(lint): apply storybook eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import storybook from 'eslint-plugin-storybook'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -17,6 +18,12 @@ export default tseslint.config(
       ...tseslint.configs.recommended,
     ],
     files: ['**/*.test.{ts,tsx}'],
+  },
+  {
+    extends: [
+      ...storybook.configs['flat/recommended'],
+    ],
+    files: ['**/*.stories.{ts,tsx}'],
   },
   {
     extends: [
