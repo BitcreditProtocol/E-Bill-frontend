3ac906065962bdef554aeb853b1995fdf2d3e083
mjkeaton <michaeljkeaton@protonmail.com>
2024-12-03 13:42:43 +0100

    chore(storybook): add basic story for Input component

---
+++ src/components/ui/input.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta,  StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import { Input } from './input';
+
+const meta = {
+  title: 'Components/Input',
+  component: Input,
+  args: { onClick: fn() },
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    label: "label",
+  }
+};
+
+export const Required: Story = {
+  args: {
+    label: "label",
+    required: true,
+  }
+};
