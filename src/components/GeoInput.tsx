03d64114692cd04c917af13bd61f9a9f20462fe3
mjkeaton <michaeljkeaton@protonmail.com>
2024-12-03 16:26:13 +0100

    chor(#110): initialize provider with locale

--- src/components/GeoInput.tsx
+++ src/components/GeoInput.tsx
@@ -1,16 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Input, type InputProps } from "./ui/input";
 import { OpenStreetMapProvider } from 'leaflet-geosearch';
 import { SearchResult } from "leaflet-geosearch/dist/providers/provider.js";
 import { RawResult } from "leaflet-geosearch/dist/providers/openStreetMapProvider.js";
-
-const provider = new OpenStreetMapProvider();
+import { useLanguage } from "@/context/language/LanguageContext";
 
 export type GeoInputProps = InputProps
 
 const GeoInput = (props : GeoInputProps) => {
+  const { locale } = useLanguage();
   const [searchTerm, setSearchTerm] = useState("");
   const [results, setResults] = useState<SearchResult<RawResult>[]>();
+  const provider = useMemo(() => {
+    return new OpenStreetMapProvider({
+      params: {
+        'accept-language': locale,
+        layer: 'address'
+      },
+    });
+  }, [locale]);
 
   useEffect(() => {
     const search = async (query: string) => provider.search({ query });
@@ -24,7 +32,7 @@ const GeoInput = (props : GeoInputProps) => {
     }, 1_000);
 
     return () => { clearTimeout(timerId) };
-  }, [searchTerm]);
+  }, [provider, searchTerm]);
 
   useEffect(() => {
     console.log(results);
