b9dd13140c8bd8194afa43cd1dc6d029e84954bb
nafisreza <bigganinafis@gmail.com>
2024-12-04 08:35:02 +0600

    wip: clear button

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useState } from "react";
+import { useRef, useState } from "react";
 import { cn } from "@/lib/utils";
 import { cva } from "class-variance-authority";
 import { useFormContext } from "react-hook-form";
@@ -12,7 +12,14 @@ export interface InputProps
   clearable?: boolean;
   hint?: string;
   inputSize?: "sm" | "md" | "lg";
-  state?: "default" | "typing" | "hover" | "disabled" | "filled" | "success" | "error";
+  state?:
+    | "default"
+    | "typing"
+    | "hover"
+    | "disabled"
+    | "filled"
+    | "success"
+    | "error";
 }
 
 const inputVariants = cva(
@@ -20,9 +27,9 @@ const inputVariants = cva(
   {
     variants: {
       size: {
-        sm: "text-xs px-4 py-3",
-        md: "text-sm p-4",
-        lg: "text-sm px-4 py-5",
+        sm: "h-[44px] text-xs px-4 py-3",
+        md: "h-[52px] text-sm p-4",
+        lg: "h-[60px] text-sm px-4 py-5",
       },
       state: {
         default: "border-[#1B0F0014] bg-elevation-200",
@@ -41,78 +48,95 @@ const inputVariants = cva(
   }
 );
 
-const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, label, required = false, id, icon, clearable, hint, inputSize, state, ...props }, ref) => {
-    const [isFocused, setIsFocused] = useState(false);
-    const [hasValue, setHasValue] = useState(false);
-    useFormContext();
+const Input = ({
+  className,
+  type,
+  label,
+  required = false,
+  id,
+  icon,
+  clearable,
+  hint,
+  inputSize,
+  state,
+  ...props
+}: InputProps) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [hasValue, setHasValue] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+  useFormContext();
 
-    const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
-      setIsFocused(false);
-      setHasValue(!!e.target.value);
-    };
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    setIsFocused(false);
+    setHasValue(!!e.target.value);
+  };
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setHasValue(!!e.target.value);
-    };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHasValue(!!e.target.value);
+  };
 
-    const clearField = () => {
-      if (ref && 'current' in ref && ref.current) {
-        ref.current.value = '';
-        setHasValue(false);
-      }
-    };
+  const clearField = () => {
+    if (inputRef.current) {
+      inputRef.current.value = "";
+      setHasValue(false);
+    }
+  };
 
-    return (
-      <div className="relative">
-        <div className={cn(inputVariants({ size: inputSize, state }), className)}>
-          {icon && <span>{icon}</span>}
-          <input
-            type={type}
-            id={id}
-            className={`flex font-medium bg-transparent outline-none ${isFocused || hasValue ? "pt-1" : ""}`}
-            ref={ref}
-            onFocus={() => { setIsFocused(true); }}
-            onBlur={handleBlur}
-            onChange={handleChange}
-            {...props}
-          />
-          {clearable && hasValue && (
-            <button type="button" onClick={clearField} className="absolute right-2">
-              <XIcon className="h-2.5 w-2.5 text-text-300" />
-            </button>
-          )}
-        </div>
-        <label
-          htmlFor={id}
-          className={cn(
-            "absolute left-4 transition-all duration-200 ease-out flex items-center text-text-300 font-medium",
-            inputSize === 'sm' ? 'text-xs' : 'text-sm',
-            isFocused || hasValue
-              ? "top-1 text-[12px] text-text-200"
-             : "top-1/2 -translate-y-1/2"
-          )}
-        >
-          {label}
-          {required && (
-            <span
-              className={
-                isFocused || hasValue ? "text-text-200" : "text-signal-error"
-              }
-            >
-              *
-            </span>
-          )}
-        </label>
-        {hint && (
-          <div className="text-xs text-text-200 mt-[2px]">
-            {hint}
-          </div>
+  return (
+    <div className="relative">
+      <div className={cn(inputVariants({ size: inputSize, state }), className)}>
+        {icon && <span>{icon}</span>}
+        <input
+          type={type}
+          id={id}
+          className={`flex font-medium bg-transparent outline-none ${
+            isFocused || hasValue ? "pt-3" : ""
+          }`}
+          ref={inputRef}
+          onFocus={() => {
+            setIsFocused(true);
+          }}
+          onBlur={handleBlur}
+          onChange={handleChange}
+          {...props}
+        />
+        {clearable && hasValue && (
+          <button
+            type="button"
+            onClick={clearField}
+            className="absolute right-4"
+          >
+            <XIcon className="h-5 w-5 text-text-300" />
+          </button>
         )}
       </div>
-    );
-  }
-);
+      <label
+        htmlFor={id}
+        className={cn(
+          "absolute left-4 transition-all duration-200 ease-out flex items-center text-text-300 font-medium",
+          inputSize === "sm" ? "text-xs" : "text-sm",
+          isFocused || hasValue
+            ? inputSize === "sm"
+              ? "top-1 text-xs text-text-200"
+              : "top-2 text-xs text-text-200"
+            : "top-1/2 -translate-y-1/2"
+        )}
+      >
+        {label}
+        {required && (
+          <span
+            className={
+              isFocused || hasValue ? "text-text-200" : "text-signal-error"
+            }
+          >
+            *
+          </span>
+        )}
+      </label>
+      {hint && <div className="text-xs text-text-200 mt-[2px]">{hint}</div>}
+    </div>
+  );
+};
 Input.displayName = "Input";
 
 export { Input };
