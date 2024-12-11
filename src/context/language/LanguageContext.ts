5a13606ae2e53c5c1d26d62f0331af7309fc15f5
mjkeaton <michaeljkeaton@protonmail.com>
2024-12-11 16:32:05 +0100

    chore: ability to control if locale is initialized from browser lang

--- src/context/language/LanguageContext.ts
+++ src/context/language/LanguageContext.ts
@@ -1,6 +1,14 @@
 import { createContext, useContext } from "react";
 
-const DEFAULT_LOCALE = "en-US";
+/**
+ * Crowdin "pseudo-language" for In-Context tooling
+ * See: https://support.crowdin.com/developer/in-context-localization/
+ */
+const CROWDIN_PSEUDO_LOCALE = "ach-UG";
+
+const DEFAULT_LOCALE_PROD = "en-US";
+const DEFAULT_LOCALE_DEV = CROWDIN_PSEUDO_LOCALE;
+const DEFAULT_LOCALE = import.meta.env.DEV ? DEFAULT_LOCALE_DEV : DEFAULT_LOCALE_PROD;
 
 export type LanguageContextType = {
   locale: string;
