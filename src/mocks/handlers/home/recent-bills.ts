9977b5989ce65563699100c4f787115c65256fa3
Guilherme Salomão <cryptosalomao@gmail.com>
2024-12-11 00:04:58 -0300

    Integrate API on home screen (#123)
    * chore: refactor home screen
    
    * chore: create bottom navigation layout
    
    * chore: move bottom navigation to layout
    
    * chore: add request mockup handlers

---
+++ src/mocks/handlers/home/recent-bills.ts
@@ -0,0 +1,37 @@
+import { http, delay, HttpResponse } from "msw";
+import type { Bill } from "@/types/bill";
+
+const mockedBills = [
+  {
+    id: "1",
+    name: "Hayek Ltd.",
+    amount_numbers: 100,
+    currency_code: "USD",
+    date_of_issue: "2021-01-01",
+  },
+  {
+    id: "2",
+    name: "Mises Inc.",
+    amount_numbers: 50,
+    currency_code: "USD",
+    date_of_issue: "2021-01-02",
+  },
+  {
+    id: "3",
+    name: "Rothbard Corp.",
+    amount_numbers: 1075.9,
+    currency_code: "USD",
+    date_of_issue: "2021-01-03",
+  },
+];
+
+type BillsResponse = Bill[];
+
+export const recentBills = http.get<never, never, BillsResponse, "/bills">(
+  "/bills",
+  async () => {
+    await delay(1000);
+
+    return HttpResponse.json(mockedBills);
+  }
+);
