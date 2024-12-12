9650bca0b7922654614eab936510669f1f980467
mjkeaton <michaeljkeaton@protonmail.com>
2024-12-12 12:11:18 +0100

    feat: add simple settings screen

---
+++ src/pages/settings/LocaleDropdown.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { FormattedMessage, useIntl } from "react-intl";
+import { Select, SelectTrigger, SelectContent, SelectValue, SelectGroup, SelectItem } from "@/components/ui/select";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
+
+interface LocaleDropdownProps {
+  value?: string
+  values: string[]
+  onChange: (locale: string) => void;
+}
+
+export function LocaleDropdown({ value, values, onChange }: LocaleDropdownProps) {
+  const intl = useIntl();
+  return (
+    <Select onValueChange={onChange} value={value}>
+      <SelectTrigger id="localeInput" label={intl.formatMessage({
+          id: "Locale",
+          defaultMessage: "Locale",
+          description: "Title for locale dropdown"
+        })} >
+        <SelectValue placeholder={intl.formatMessage({
+          id: "Select locale",
+          defaultMessage: "Select locale",
+          description: "Placeholder for locale dropdown"
+        })} />
+      </SelectTrigger>
+      <SelectContent>
+        <ScrollArea className="h-[10rem]">
+          <SelectGroup>
+            {values.map(it => {
+              return (
+                <SelectItem key={it} value={it}>
+                  <FormattedMessage
+                    id={`locale.${it}`}
+                    defaultMessage={it}
+                    description={`Locale dropdown menu value for locale ${it}`}
+                  />
+                </SelectItem>
+              )
+            })}
+          </SelectGroup>
+        </ScrollArea>
+      </SelectContent>
+    </Select>
+  );
+}
---
+++ src/pages/settings/index.tsx
@@ -0,0 +1,31 @@
+import { useLanguage } from "@/context/language/LanguageContext";
+import { ChevronLeftIcon } from "lucide-react";
+import { FormattedMessage } from "react-intl";
+import { LocaleDropdown } from "./LocaleDropdown";
+
+
+export default function Settings() {
+  const lang = useLanguage();
+
+  return (
+    <div className="flex flex-col gap-6 w-full min-h-fit h-screen py-4 px-5">
+      <div className="w-full">
+        <button className="flex items-center justify-center w-8 h-8 bg-[#1B0F00]/20 rounded-full border-[1px] border-[#1B0F00]/6">
+          <ChevronLeftIcon width={16} strokeWidth={1} color="#1B0F00" />
+        </button>
+      </div>
+
+      <div className="flex flex-col gap-2 items-center">
+        <h1 className="font-sans font-medium text-2xl tracking-tight mb-0">
+          <FormattedMessage
+            id="Settings"
+            defaultMessage="Settings"
+            description="Header copy for Settings page"
+          />
+        </h1>
+      </div>
+
+      <LocaleDropdown value={lang.locale} values={lang.availableLocales()} onChange={lang.setLocale} />
+    </div>
+  );
+}
