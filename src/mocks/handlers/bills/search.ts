3b8451c6929c957bf0e6ab90c5c6e17cecc4a9fe
Guilherme Salomão <cryptosalomao@gmail.com>
2024-12-13 07:16:38 -0300

    Bills API integration (#153)
    * chore: refactor bills overview page
    
    * chore: integrate more endpoints
    
    * chore: fix issues
    
    * fix: loading status
    
    * chore: fix type issues
    
    * chore: fix type issues
    
    * chore: fix type issues
    
    * Update public/mockServiceWorker.js
    
    Co-authored-by: Michael Keaton <michaeljkeaton@protonmail.com>
    
    ---------
    
    Co-authored-by: Michael Keaton <michaeljkeaton@protonmail.com>

---
+++ src/mocks/handlers/bills/search.ts
@@ -0,0 +1,143 @@
+import { http, HttpResponse } from "msw";
+import { SEARCH_BILLS } from "@/constants/endpoints";
+import type { Bill } from "@/types/bill";
+
+type SearchBillsResponse = Pick<
+  Bill,
+  | "bill_name"
+  | "role"
+  | "payer"
+  | "holder"
+  | "payee"
+  | "drawer"
+  | "sum"
+  | "issue_date"
+>;
+
+const data: SearchBillsResponse[] = [
+  {
+    bill_name: "xxs1",
+    role: "payee",
+    payer: {
+      name: "Mises Inc.",
+    },
+    holder: {
+      name: "Rothbard Corp.",
+    },
+    payee: {
+      name: "Rayek Ltd.",
+    },
+    drawer: {
+      name: "Mises 2 Inc.",
+    },
+    sum: {
+      amount: "100",
+      currency: "USD",
+    },
+    issue_date: "2021-01-01",
+  },
+  {
+    bill_name: "xxs",
+    role: "payer",
+    payer: {
+      name: "Rothbard Corp.",
+    },
+    holder: {
+      name: "Hayek Ltd.",
+    },
+    payee: {
+      name: "Mises Corp",
+    },
+    drawer: {
+      name: "Rothbard Corp.",
+    },
+    sum: {
+      amount: "50",
+      currency: "USD",
+    },
+    issue_date: "2021-01-02",
+  },
+  {
+    bill_name: "axs",
+    role: "drawer",
+    payer: {
+      name: "Hayek Ltd.",
+    },
+    holder: {
+      name: "Mises Inc.",
+    },
+    payee: {
+      name: "Mises Corp",
+    },
+    drawer: {
+      name: "Hayek Ltd.",
+    },
+    sum: {
+      amount: "1075.9",
+      currency: "USD",
+    },
+    issue_date: "2021-01-03",
+  },
+];
+
+type SearchBillsFilter = {
+  search_term?: string;
+  date_range?: {
+    from: string;
+    to: string;
+  };
+  role?: string;
+  currency?: string;
+};
+
+const filterBills = (
+  data: SearchBillsResponse[],
+  filter: SearchBillsFilter
+): SearchBillsResponse[] => {
+  return data.filter((bill) => {
+    const { search_term, date_range, role, currency } = filter;
+
+    const matchesSearchTerm = search_term
+      ? Object.entries(bill).some(([, value]) => {
+          if (typeof value === "object") {
+            return Object.values(value).some(
+              (innerValue) =>
+                typeof innerValue === "string" &&
+                innerValue.toLowerCase().includes(search_term.toLowerCase())
+            );
+          }
+          return (
+            typeof value === "string" &&
+            value.toLowerCase().includes(search_term.toLowerCase())
+          );
+        })
+      : true;
+
+    const matchesDateRange =
+      date_range && date_range.from !== "" && date_range.to !== ""
+        ? new Date(bill.issue_date) >= new Date(date_range.from) &&
+          new Date(bill.issue_date) <= new Date(date_range.to)
+        : true;
+
+    const matchesRole = role && role !== "all" ? bill.role === role : true;
+
+    const matchesCurrency = currency ? bill.sum.currency === currency : true;
+
+    return (
+      matchesSearchTerm && matchesDateRange && matchesRole && matchesCurrency
+    );
+  });
+};
+
+export const searchBills = http.post<
+  never,
+  never,
+  SearchBillsResponse[],
+  typeof SEARCH_BILLS
+>(SEARCH_BILLS, async ({ request }) => {
+  const { filter } = await request.json();
+
+  const filteredData = filterBills(data, filter);
+
+  return HttpResponse.json(filteredData);
+});
--- src/services/bill.ts
+++ src/services/bill.ts
@@ -1,8 +1,19 @@
-import { Bill } from "@/types/bill";
+import { SEARCH_BILLS } from "@/constants/endpoints";
+import type { Bill } from "@/types/bill";
 
 type BillsResponse = Bill[];
 
 export const getRecentBills = async () => {
+  const response = await fetch("/recent-bills");
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch bills");
+  }
+
+  return response.json() as Promise<BillsResponse>;
+};
+
+export const getBills = async () => {
   const response = await fetch("/bills");
 
   if (!response.ok) {
@@ -10,4 +21,39 @@ export const getRecentBills = async () => {
   }
 
   return response.json() as Promise<BillsResponse>;
+};
+
+type SearchBillsPayload = {
+  filter: {
+    search_term?: string;
+    date_range?: {
+      from: string;
+      to: string;
+    };
+    role?: string;
+    currency?: string;
+  };
+};
+
+type SearchBillsResponse = Pick<
+  Bill,
+  "bill_name" | "role" | "drawer" | "sum" | "issue_date"
+>[];
+
+export const searchBills = async (payload: SearchBillsPayload) => {
+  const response = await fetch(SEARCH_BILLS, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to search bills: ${response.statusText}`);
+  }
+
+  const data = (await response.json()) as Promise<SearchBillsResponse>;
+
+  return data;
 };
