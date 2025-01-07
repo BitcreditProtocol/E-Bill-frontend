f9283b233b7c9fb21f3f6db9a18c820bc9869205
mjkeaton <michaeljkeaton@protonmail.com>
2025-01-07 17:21:10 +0100

    chore(build): add buildCommand to vercel config

--- vercel.sh
+++ vercel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
  
-if [[ $VERCEL_ENV == "production"  ]] ; then 
+if [[ "$VERCEL_ENV" == "production"  ]]; then
   npm run build
-else if [[ $VERCEL_ENV == "crowdin"  ]] ; then 
+elif [[ "$VERCEL_ENV" == "crowdin"  ]]; then
   npm run build:crowdin
 else 
   npm run build:dev
