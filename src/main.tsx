fef46fdd65949b5b021cfb53d746a9209b034a1a
mjkeaton <michaeljkeaton@protonmail.com>
2025-01-08 14:43:41 +0100

    chore: remove duplicate Toaster component

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, Outlet, RouteObject, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { Toaster } from "./components/ui/toaster";
 import LanguageProvider from "./context/language/LanguageProvider";
 
 import DefaultLayout from "./layouts/Default";
@@ -213,7 +212,6 @@ void prepare().then(() => {
               v7_startTransition: true,
             }}
           />
-          <Toaster />
         </LanguageProvider>
       </QueryClientProvider>
     </StrictMode>
