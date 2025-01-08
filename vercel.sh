fcf24229e470a43e0124ed125484d680f127c680
mjkeaton <michaeljkeaton@protonmail.com>
2025-01-08 11:37:09 +0100

    fix(vercel): use VERCEL_URL

--- vercel.sh
+++ vercel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
-if [[ "$VERCEL_PROJECT_PRODUCTION_URL" == "e-bill-frontend-crowdin.vercel.app"  ]]; then
+if [[ "$VERCEL_URL" == "e-bill-frontend-crowdin.vercel.app"  ]]; then
   npm run build:crowdin
 else 
   npm run build:dev
