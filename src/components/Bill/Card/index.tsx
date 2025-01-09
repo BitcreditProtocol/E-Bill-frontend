1a8c1b4060fa6f05f9ba3fd7d76d1d0f3bec0958
Guilherme Salomão <cryptosalomao@gmail.com>
2025-01-09 11:56:13 -0300

    wip

--- src/components/Bill/Card/index.tsx
+++ src/components/Bill/Card/index.tsx
@@ -1,13 +1,13 @@
 import { FormattedMessage, useIntl } from "react-intl";
-import { CheckCircleIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, PaperclipIcon } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 
 function Separators() {
   return (
     <div className="flex flex-col gap-0.5">
-      {Array.from({ length: 5 }).map((_, index) => (
-        <Separator key={index} className="bg-divider-300" />
-      ))}
+      <Separator className="bg-divider-300" />
+      <Separator className="bg-divider-300" />
+      <Separator className="bg-divider-300" />
     </div>
   );
 }
@@ -53,112 +53,133 @@ export default function Card({
   const placeOfPayment = `${city_of_payment}, ${country_of_payment}`;
 
   return (
-    <div className="flex flex-col border-[1px] border-divider-50 rounded-[16px]">
-      <span className="py-3 px-5 text-text-300 text-sm font-medium">
-        {issuingInformation}
-      </span>
+    <div className="flex flex-col border border-divider-50 rounded-xl">
+      <div className="flex flex-col p-5 bg-elevation-200 rounded-t-xl">
+        <div className="flex justify-between">
+          <div className="flex flex-col gap-1">
+            <span className="text-text-200 text-sm font-medium">
+              {issuingInformation}
+            </span>
 
-      <div className="flex flex-col gap-3 py-6 px-5 bg-elevation-200 rounded-b-[16px]">
-        <div className="flex flex-col gap-1">
-          <h2 className="text-text-300 text-base font-medium">
-            <FormattedMessage
-              id="Against this bill of exchange"
-              defaultMessage="Against this bill of exchange"
-              description="Heading for bill preview page"
+            <h2 className="text-text-300 text-base font-medium">
+              <FormattedMessage
+                id="Against this bill of exchange"
+                defaultMessage="Against this bill of exchange"
+                description="Heading for bill preview page"
+              />
+            </h2>
+          </div>
+
+          <PaperclipIcon className="text-text-300 w-5 h-5 stroke-1" />
+        </div>
+
+        <div className="flex flex-col gap-3 bg-elevation-200 rounded-b-[16px]">
+          <Separators />
+
+          <div className="flex flex-col gap-3 py-3 bg-elevation-50 border-[1px] border-divider-75 rounded-t-[12px] rounded-b-[16px]">
+            <Property
+              label={intl.formatMessage({
+                id: "bill.card.payOn",
+                defaultMessage: "Pay on",
+                description: "Pay on property for bill card",
+              })}
+              value={maturity_date}
             />
-          </h2>
+            <Separator />
+
+            <Property
+              label={intl.formatMessage({
+                id: "bill.card.drawee",
+                defaultMessage: "Drawee",
+                description: "Drawee property for bill card",
+              })}
+              value={drawee}
+            />
+            <Separator />
+
+            <Property label="The sum of" value="0.10000000" />
+            <Separator />
+
+            <Property
+              label={intl.formatMessage({
+                id: "bill.card.payer",
+                defaultMessage: "Payer",
+                description: "Payer property for bill card",
+              })}
+              value={payer}
+            />
+            <Separator />
+
+            <Property
+              label={intl.formatMessage({
+                id: "bill.card.drawer",
+                defaultMessage: "Drawer",
+                description: "Drawer property for bill card",
+              })}
+              value={drawer}
+            />
+            <Separator />
+
+            <Property
+              label={intl.formatMessage({
+                id: "bill.card.placeOfPayment",
+                defaultMessage: "Place of payment",
+                description: "Place of payment property for bill card",
+              })}
+              value={placeOfPayment}
+            />
+          </div>
+
+          <div className="flex flex-col gap-3">
+            <Separators />
+
+            <span className="text-text-300 text-sm font-medium">
+              <FormattedMessage
+                id="bill.card.noProtest"
+                defaultMessage="No protest. Value received."
+                description="No protest copy for bill card"
+              />
+            </span>
+          </div>
         </div>
+      </div>
 
-        <Separators />
-
-        <div className="flex flex-col gap-3 py-3 bg-elevation-50 border-[1px] border-divider-75 rounded-t-[12px] rounded-b-[16px]">
-          <Property
-            label={intl.formatMessage({
-              id: "bill.card.payOn",
-              defaultMessage: "Pay on",
-              description: "Pay on property for bill card",
-            })}
-            value={maturity_date}
-          />
-          <Separator />
-
-          <Property
-            label={intl.formatMessage({
-              id: "bill.card.drawee",
-              defaultMessage: "Drawee",
-              description: "Drawee property for bill card",
-            })}
-            value={drawee}
-          />
-          <Separator />
-
-          <Property label="The sum of" value="0.10000000" />
-          <Separator />
-
-          <Property
-            label={intl.formatMessage({
-              id: "bill.card.payer",
-              defaultMessage: "Payer",
-              description: "Payer property for bill card",
-            })}
-            value={payer}
-          />
-          <Separator />
-
-          <Property
-            label={intl.formatMessage({
-              id: "bill.card.placeOfPayment",
-              defaultMessage: "Place of payment",
-              description: "Place of payment property for bill card",
-            })}
-            value={placeOfPayment}
-          />
-          <Separator />
-
-          <Property
-            label={intl.formatMessage({
-              id: "bill.card.drawer",
-              defaultMessage: "Drawer",
-              description: "Drawer property for bill card",
-            })}
-            value={drawer}
-          />
-        </div>
-
-        <div className="flex flex-col gap-3">
-          <span className="text-text-300 text-sm font-medium">
+      <div className="flex items-center justify-between gap-3 py-4 px-5">
+        <div className="flex gap-1">
+          <span className="text-text-200 text-xs font-normal">
             <FormattedMessage
-              id="No protest"
-              defaultMessage="No protest"
-              description="No protest copy for bill preview page"
+              id="bill.card.status"
+              defaultMessage="Status:"
+              description="Status property for bill card"
             />
           </span>
 
-          <Separators />
+          <div className="flex items-center gap-1">
+            <CheckIcon className="text-text-300 w-3 h-3 stroke-3" />
 
-          <div className="flex flex-col gap-1.5">
-            <span className="text-text-200 text-xs font-normal">
+            <span className="text-text-300 text-xs font-normal">
               <FormattedMessage
-                id="Invoice"
-                defaultMessage="Invoice"
-                description="Invoice copy for bill preview page"
+                id="bill.card.accepted"
+                defaultMessage="Accepted"
+                description="Accepted status for bill card"
               />
             </span>
-
-            <div className="flex h-12 w-full p-3 bg-elevation-50 border-[1px] border-divider-75 rounded-[12px]">
-              <div className="flex gap-1">
-                <span className="text-sm font-medium text-text-300">
-                  Invoice.pdf
-                </span>
-
-                <div className="flex items-center gap-1">
-                  <span className="text-xs text-[#1B0F0080]">200 KB</span>
-                  <CheckCircleIcon color="#006F29" width={16} />
-                </div>
-              </div>
-            </div>
           </div>
         </div>
+
+        <button className="flex items-center gap-1">
+          <span className="text-text-200 text-xs font-normal">
+            <FormattedMessage
+              id="bill.card.endorsements"
+              defaultMessage="Endorsements"
+              description="Endorsements button for bill card"
+            />
+          </span>
+
+          <span className="text-text-300 text-xs font-medium">(12)</span>
+
+          <ChevronRightIcon className="text-text-300 w-5 h-5 ml-0.5 stroke-1" />
+        </button>
       </div>
     </div>
   );
--- src/pages/Bill.tsx
+++ src/pages/Bill.tsx
@@ -1,30 +1,9 @@
-import { FormattedMessage } from "react-intl";
-import {
-  ChevronDownIcon,
-  ChevronLeftIcon,
-  MoveRightIcon,
-  PaperclipIcon,
-  PrinterIcon,
-} from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
-
-function BillProperty({ label, value }: { label: string; value: string }) {
-  return (
-    <div className="flex justify-between items-start px-5">
-      <span className="flex-1 text-text-200 text-xs font-normal pt-0.5">
-        {label}
-      </span>
-      <span className="flex-1 text-text-300 text-sm font-medium text-right">
-        {value}
-      </span>
-    </div>
-  );
-}
+import { ChevronLeftIcon, PaperclipIcon, PrinterIcon } from "lucide-react";
+import Card from "@/components/Bill/Card";
 
 export default function Bill() {
   return (
-    <div className="flex flex-col justify-between gap-6 w-full min-h-fit h-screen py-4 px-5">
+    <div className="flex flex-col gap-6 w-full min-h-fit h-screen py-4 px-5">
       <div className="flex justify-between">
         <button className="flex items-center justify-center w-8 h-8 bg-[#1B0F00]/20 rounded-full border-[1px] border-[#1B0F00]/6">
           <ChevronLeftIcon width={16} strokeWidth={1} color="#1B0F00" />
@@ -40,65 +19,17 @@ export default function Bill() {
         </div>
       </div>
 
-      <div className="flex flex-col flex-1 gap-6">
-        <div className="flex flex-col gap-1">
-          <span className="text-text-200 text-sm font-medium">
-            Stockholm, SE, 03-Nov-2024
-          </span>
-          <h2 className="text-text-300 text-xl font-medium">
-            <FormattedMessage
-              id="Against this bill of exchange"
-              defaultMessage="Against this bill of exchange"
-              description="Heading for bill visualization page"
-            />
-          </h2>
-        </div>
-
-        <div className="flex flex-col gap-3 pt-3 border-[1px] border-divider-75 rounded-t-[12px] rounded-b-[16px]">
-          <BillProperty label="Pay on" value="03-Nov-2024" />
-          <Separator />
-          <BillProperty label="To the order of" value="Ashwin Merle" />
-          <Separator />
-          <BillProperty label="The sum of" value="03-Nov-2024" />
-          <Separator />
-          <BillProperty
-            label="Payer"
-            value="Danube Water Shipping and Transport Aktiengesellschaft"
-          />
-          <Separator />
-          <BillProperty label="Place of payment" value="AT, Vienna" />
-          <Separator />
-          <BillProperty label="Drawer" value="Bitcredit Austria" />
-          <button className="flex items-center justify-between bg-elevation-200 py-3 px-5 border-t-[1px] border-t-divider-75 rounded-b-[16px]">
-            <div className="flex gap-1 items-center">
-              <span className="text-xs font-normal">Endorsements</span>
-              <span className="text-[10px] font-medium">(12)</span>
-            </div>
-            <MoveRightIcon size={20} color="#1B0F00" strokeWidth={1} />
-          </button>
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-3">
-        <Button className="w-full bg-text-300 text-white font-medium rounded-[8px] py-3 px-6">
-          <FormattedMessage
-            id="Request to accept"
-            defaultMessage="Request to accept"
-            description="Action to request to accept the bill"
-          />
-        </Button>
-        <Button
-          className="gap-2.5 w-full text-text-300 bg-transparent font-medium border-text-300 rounded-[8px] py-3 px-6 hover:bg-transparent"
-          variant="outline"
-        >
-          <FormattedMessage
-            id="Endorse"
-            defaultMessage="Endorse"
-            description="Action to endorse the bill"
-          />
-          <ChevronDownIcon size={20} color="#1B0F00" strokeWidth={1} />
-        </Button>
-      </div>
+      <Card
+        city_of_issuing="São Paulo"
+        country_of_issuing="BR"
+        issue_date="08-Jan-2025"
+        maturity_date="11-Jan-2025"
+        payer="John Doe"
+        drawee="Jane Doe"
+        drawer="John Jon"
+        city_of_payment="São Paulo"
+        country_of_payment="BR"
+      />
     </div>
   );
 }
