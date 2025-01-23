91201acee4733d941479c605171d9ed5a6d3e76f
Guilherme Salomão <cryptosalomao@gmail.com>
2025-01-23 09:27:59 -0300

    wip

--- src/pages/contacts/Overview.tsx
+++ src/pages/contacts/Overview.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FormattedMessage, useIntl } from "react-intl";
 import { ChevronRightIcon, PlusIcon, SearchIcon } from "lucide-react";
 
@@ -43,9 +43,36 @@ function Loader() {
     <div className="flex flex-col gap-3 w-full">
       <Skeleton className="w-full h-4 bg-elevation-200 rounded-lg" />
       {Array.from({ length: 3 }, (_, index) => (
-        <Skeleton key={index} className="w-full h-16 bg-elevation-200 rounded-lg" />
+        <Skeleton
+          key={index}
+          className="w-full h-16 bg-elevation-200 rounded-lg"
+        />
       ))}
     </div>
+  );
+}
+
+function Create() {
+  return (
+    <Link to={routes.CREATE_CONTACT}>
+      <div className="flex items-center justify-between py-6 px-3 bg-elevation-200 border border-divider-75 rounded-xl">
+        <div className="flex items-center gap-2">
+          <div className="flex items-center justify-center h-6 w-6 bg-brand-200 rounded-full">
+            <PlusIcon className="text-brand-50 h-4 w-4 stroke-1" />
+          </div>
+
+          <span className="text-text-300 text-sm font-medium leading-5">
+            <FormattedMessage
+              id="contacts.create"
+              defaultMessage="New contact"
+              description="Create new contact"
+            />
+          </span>
+        </div>
+
+        <ChevronRightIcon className="text-text-300 h-4 w-4 stroke-1" />
+      </div>
+    </Link>
   );
 }
 
@@ -53,37 +80,42 @@ export default function Overview() {
   const intl = useIntl();
   const navigate = useNavigate();
   const queryClient = useQueryClient();
-  const { isPending, isSuccess, data } = useQuery({
-    queryKey: ["contacts"],
-    queryFn: getContacts,
-  }, queryClient);
+  const { isPending, isSuccess, data } = useQuery(
+    {
+      queryKey: ["contacts"],
+      queryFn: getContacts,
+    },
+    queryClient
+  );
 
   const {
     data: searchData,
     isPending: isSearchPending,
     isSuccess: isSearchSuccess,
-    mutate
-  } = useMutation({
-    mutationFn: () => searchContacts({ 
-      filter: {
-        search_term: searchTerm,
-        types: typeFilters.length === 0 ? undefined : typeFilters,
-      }
-    })
-  }, queryClient);
+    mutate,
+  } = useMutation(
+    {
+      mutationFn: () =>
+        searchContacts({
+          filter: {
+            search_term: searchTerm,
+            types: typeFilters.length === 0 ? undefined : typeFilters,
+          },
+        }),
+    },
+    queryClient
+  );
 
   const [values, setValues] = useState<Contact[]>(data?.contacts || []);
-  const [typeFilters, setTypeFilters] = useState<Contact['type'][]>([]);
+  const [typeFilters, setTypeFilters] = useState<Contact["type"][]>([]);
   const [searchTerm, setSearchTerm] = useState("");
-
-  const goToCreate = () => {
-    navigate(routes.CREATE_CONTACT);
-  };
 
   const __dev_toggleEmptyScenario = () => {
     navigate({
       pathname: ".",
-      search: window.location.search.includes("scenario=empty") ? "" : "scenario=empty",
+      search: window.location.search.includes("scenario=empty")
+        ? ""
+        : "scenario=empty",
     });
     navigate(0); // triggers reload
   };
@@ -102,11 +134,18 @@ export default function Overview() {
 
   return (
     <div className="flex flex-col items-center gap-6 w-full min-h-fit h-screen py-4 px-5">
-      {import.meta.env.DEV && (<>
-        <Button size="xxs" variant="destructive" className="absolute top-1 right-1" onClick={__dev_toggleEmptyScenario} >
-          [dev] Toggle empty scenario
-        </Button>
-      </>)}
+      {import.meta.env.DEV && (
+        <>
+          <Button
+            size="xxs"
+            variant="destructive"
+            className="absolute top-1 right-1"
+            onClick={__dev_toggleEmptyScenario}
+          >
+            [dev] Toggle empty scenario
+          </Button>
+        </>
+      )}
 
       <div className="flex flex-col gap-3 w-full">
         <h1 className="text-text-300 text-xl font-medium">
@@ -117,59 +156,58 @@ export default function Overview() {
           />
         </h1>
 
-        <Search placeholder={intl.formatMessage({
+        <Search
+          placeholder={intl.formatMessage({
             id: "Name, address, email",
             defaultMessage: "Name, address, email...",
             description: "Placeholder text for contacts search input",
           })}
           onChange={setSearchTerm}
-          onSearch={() => { 
+          onSearch={() => {
             mutate();
           }}
         />
-        <TypeFilter multiple values={typeFilters} onChange={(types) => {
-          setTypeFilters(types);
-          mutate();
-        }} />
+        <TypeFilter
+          multiple
+          values={typeFilters}
+          onChange={(types) => {
+            setTypeFilters(types);
+            mutate();
+          }}
+        />
       </div>
 
       <div className="flex flex-col gap-4 w-full">
-        <button
-          className="flex items-center justify-between w-full"
-          onClick={goToCreate}
-        >
-          <div className="flex items-center gap-2">
-            <div className="flex items-center justify-center w-8 h-8 bg-brand-200 p-2 rounded-full">
-              <PlusIcon className="w-4 h-4 text-brand-50" />
-            </div>
-
-            <span className="text-text-300 text-base font-medium">
-              <FormattedMessage
-                id="New Contact"
-                defaultMessage="New contact"
-                description="New contact button"
-              />
-            </span>
-          </div>
-          <ChevronRightIcon className="w-6 h-6 text-text-300" />
-        </button>
+        <Create />
 
         <Separator className="bg-divider-75" />
       </div>
 
-      {isPending || isSearchPending ? (<>
-        <Loader />
-      </>) : (<>
-        {values.length === 0 && data?.contacts.length === 0 ? (<>
-          <EmptyList />
-        </>) : (<>
-        {values.length === 0 ? (<>
-          <NoResults />
-        </>) : (<>
-            <List values={values} />
-          </>)}
-        </>)}
-      </>)}
+      {isPending || isSearchPending ? (
+        <>
+          <Loader />
+        </>
+      ) : (
+        <>
+          {values.length === 0 && data?.contacts.length === 0 ? (
+            <>
+              <EmptyList />
+            </>
+          ) : (
+            <>
+              {values.length === 0 ? (
+                <>
+                  <NoResults />
+                </>
+              ) : (
+                <>
+                  <List values={values} />
+                </>
+              )}
+            </>
+          )}
+        </>
+      )}
     </div>
   );
 }
