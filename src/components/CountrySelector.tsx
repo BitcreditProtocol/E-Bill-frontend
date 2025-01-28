057debb7c06c36b8211a42e04e5f61943b7d32b5
Guilherme Salomão <cryptosalomao@gmail.com>
2025-01-28 11:12:09 -0300

    contacts flow fixes (#242)
    * wip
    
    * chore: update contact view flow
    
    * chore: update contact creation flow
    
    * wip
    
    * chore: fix country selector rendering; place all registration steps into the same file
    
    * chore: move entire contact view into single file
    
    * wip
    
    * wip
    
    * wip
    
    * chore: updates to edit flow
    
    * chore: integrate contact view endpoint
    
    * chore: fix routes
    
    * chore: type corrections and more mocks
    
    * chore: more typedefs
    
    * wip

---
+++ src/components/CountrySelector.tsx
@@ -0,0 +1,54 @@
+import { useMemo } from "react";
+import { EarthIcon } from "lucide-react";
+import {
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+} from "@/components/ui/select";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { COUNTRIES } from "@/constants/countries";
+
+export default function CountrySelector({
+  label,
+  value,
+  callback,
+}: {
+  label: string;
+  value?: string;
+  callback: (value: string) => void;
+}) {
+  const renderCountriesList = useMemo(
+    () =>
+      Object.entries(COUNTRIES).map(([code, country]) => (
+        <SelectItem key={code} value={code}>
+          {country}
+        </SelectItem>
+      )),
+    []
+  );
+
+  return (
+    <Select
+      onValueChange={(value) => {
+        callback(value);
+      }}
+      value={value}
+      required
+    >
+      <SelectTrigger
+        icon={<EarthIcon className="text-text-300 h-5 w-5 stroke-1" />}
+        label={label}
+      >
+        <SelectValue className="!text-red-200" placeholder={label} />
+      </SelectTrigger>
+      <SelectContent>
+        <ScrollArea className="h-[10rem]">
+          <SelectGroup>{renderCountriesList}</SelectGroup>
+        </ScrollArea>
+      </SelectContent>
+    </Select>
+  );
+}
--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -2,5 +2,6 @@ const GET_BILLS = "/bills";
 const SEARCH_BILLS = "/bill/search";
 const GET_CONTACTS = "/contacts/list";
 const SEARCH_CONTACTS = "/contacts/search";
+const EDIT_CONTACT = "/contacts/edit";
 
-export { GET_BILLS, SEARCH_BILLS, GET_CONTACTS, SEARCH_CONTACTS };
+export { GET_BILLS, SEARCH_BILLS, GET_CONTACTS, SEARCH_CONTACTS, EDIT_CONTACT };
