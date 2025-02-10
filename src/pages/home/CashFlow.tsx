17059a7063488a55e1272e7b327bd6b04e955cae
mjkeaton <michaeljkeaton@protonmail.com>
2025-02-10 11:13:40 +0100

    chore(chart): add top navigation

--- src/pages/home/CashFlow.tsx
+++ src/pages/home/CashFlow.tsx
@@ -1,12 +1,14 @@
 import { FormattedMessage } from "react-intl";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { CalendarDaysIcon } from "lucide-react";
+import { CalendarDaysIcon, ExpandIcon } from "lucide-react";
 
 import { Skeleton } from "@/components/ui/skeleton";
 import { Button } from "@/components/ui/button";
 import ChashFlowChart from "./components/CashFlowChart";
 import { getBillsLight } from "@/services/bills";
 import { Suspense } from "react";
+import Topbar from "@/components/Topbar";
+import NavigateBack from "@/components/NavigateBack";
 
 function Loader() {
   return (
@@ -22,19 +24,27 @@ export default function CashFlow() {
 
   return (
     <div className="flex flex-col gap-6 w-full min-h-fit h-screen py-4 px-5">
-      <div className="flex flex-col gap-3">
-        <div className="flex gap-1 justify-between items-center">
-          <h2 className="text-xl font-medium text-text-300">
-            <FormattedMessage
-              id="page.cashflow.title"
-              defaultMessage="Cash flow"
-              description="Page title for Cash flow page"
-            />
-          </h2>
-
+      <Topbar
+        lead={<NavigateBack />}
+        middle={<h2 className="text-xl font-medium text-text-300">
+          <FormattedMessage
+            id="page.cashflow.title"
+            defaultMessage="Cash flow"
+            description="Page title for Cash flow page"
+          />
+        </h2>}
+        trail={
+          <Button variant="secondary" className="gap-1 p-2">
+            <ExpandIcon size={16} strokeWidth={1} color="#1B0F00" />
+          </Button>
+        }
+      />
+      
+      <div className="flex flex-col gap-1.5">
+        <div className="flex gap-1.5">
+          <div className="flex-1"></div>
           <Button variant="link" className="gap-1 text-xs text-text-300 px-0">
             <CalendarDaysIcon size={16} strokeWidth={1} color="#1B0F00" />
-
             <FormattedMessage
               id="Date range"
               defaultMessage="Date range"
@@ -42,7 +52,6 @@ export default function CashFlow() {
             />
           </Button>
         </div>
-
         <div className="flex flex-col gap-1.5">
           <Suspense fallback={<Loader />}>
             <ChashFlowChart values={data.bills} />
