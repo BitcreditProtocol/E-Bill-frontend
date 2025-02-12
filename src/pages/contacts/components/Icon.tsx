a9a0e8ba81e22352343f19376da24a5ee9d4a566
mjkeaton <michaeljkeaton@protonmail.com>
2025-02-12 16:52:24 +0100

    chore: add className to contact icon

--- src/pages/contacts/components/Icon.tsx
+++ src/pages/contacts/components/Icon.tsx
@@ -4,17 +4,19 @@ import { useLanguage } from "@/context/language/LanguageContext";
 import { cn } from "@/lib/utils";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-export type IconProps = Pick<Contact, 'type' | 'name'>;
+export type IconProps = Pick<Contact, 'type' | 'name'> & {
+  className?: string;
+};
 
-export default function Icon({ name, type }: IconProps) {
+export default function Icon({ name, type, className }: IconProps) {
   const lang = useLanguage();
 
   return (
     <Avatar className={cn("bg-brand-50 relative", {
       "rounded-full": type === ContactTypes.Person,
       "rounded-md": type !== ContactTypes.Person,
-    })}>
-      <AvatarImage src="https://randomuser.me/api/portraits" />
+    }, className)}>
+      <AvatarImage src="https://randomuser.me/api/portraits" alt="name" />
       <AvatarFallback className="bg-brand-50 text-brand-200 text-[20px] font-medium">
         {name.charAt(0).toLocaleUpperCase(lang.locale) || '?'}
       </AvatarFallback>
