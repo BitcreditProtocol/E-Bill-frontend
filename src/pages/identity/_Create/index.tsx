ac6496dca5eec77d21a77abcb8513e7bcb3c1669
Guilherme Salomão <cryptosalomao@gmail.com>
2025-02-21 10:07:50 -0300

    wip

--- src/pages/identity/_Create/index.tsx
+++ src/pages/identity/_Create/index.tsx
@@ -1,4 +1,32 @@
+import { FormProvider, useForm, useFormContext } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Page from "@/components/wrappers/Page";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+  email: z.string().email(),
+  country: z.string().min(1),
+  city: z.string().min(1),
+  zip: z.string().optional(),
+  address: z.string().min(1),
+  date_of_birth: z.string().min(1).optional(),
+  country_of_birth: z.string().min(2).optional(),
+  city_of_birth: z.string().min(1).optional(),
+  identification_number: z.string().min(1).optional(),
+
+  profile_picture_preview_url: z.string().optional(),
+  profile_picture_file_upload_id: z.string().optional(),
+  identity_document_file_upload_id: z.string().optional(),
+  identity_document_file_metadata: z
+    .object({
+      name: z.string(),
+      size: z.number(),
+    })
+    .optional(),
+});
+
+export type CreateIdentityFormSchema = z.infer<typeof formSchema>;
 
 export default function Create() {
   return <Page></Page>;
